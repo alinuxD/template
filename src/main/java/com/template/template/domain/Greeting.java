ed950ebea64e4c906a46fe4f4b6bdb04aafb919a
alinuxD <alinux_xu@outlook.com>
2017-07-02 16:35:28 +0800

    Initial commit

---
+++ src/main/java/com/template/template/domain/Greeting.java
@@ -0,0 +1,23 @@
+package com.template.template.domain;
+
+/**
+ * Created by Alinux on 2017-06-25.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
